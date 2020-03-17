c50b07097fca2a2dd39cbd6c8b50857f62925a7a
yahyabouhlal <yahya.bouhlal.b@gmail.com>
2020-03-17 22:36:23 +0100

    Create Workshop4.js

---
+++ Workshop4.js
@@ -0,0 +1,14 @@
+const formating= (numbers) =>{
+
+    numbers.forEach(element => {
+         str = numbers.join("");
+    });
+    console.log(str);
+}
+
+formating([0,1,2,3,4,5,6,7,8,9]);
+
+ var numbers =["0","1","2","3","4","4"];
+ console.log(numbers);
+ nmb = numbers.toString();
+ console.log(nmb);
